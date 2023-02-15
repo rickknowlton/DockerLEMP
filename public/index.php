d7a8da6c18df82f96b8de0fb380efc833c67923a
rickknowlton <richard.d.knowlton@gmail.com>
2023-02-15 14:53:04 -0500

    v 1.2.0

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Boilerplate PHP Site</title>
+  </head>
+  <body>
+    <h1>This is a basic PHP Boilerplate</h1>
+    <p>
+      <?php
+        echo "Today is " . date("Y-m-d") . "<br>";
+        echo "The time is " . date("h:i:sa") . "<br>";
+      ?>
+    </p>
+  </body>
+</html>
